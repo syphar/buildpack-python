3e9062525de1476781763e73f3098d46274cfb9a
Stefan Friesel <sf@cloudcontrol.de>
2013-01-24 17:06:40 +0100

    Update pip and distribute

--- vendor/virtualenv-1.7.2/virtualenv.py
+++ vendor/virtualenv-1.7.2/virtualenv.py
@@ -490,7 +490,7 @@ def _install_req(py_executable, unzip=False, distribute=False,
         source = None
     else:
         setup_fn = None
-        source = 'distribute-0.6.27.tar.gz'
+        source = 'distribute-0.6.34.tar.gz'
         project_name = 'distribute'
         bootstrap_script = DISTRIBUTE_SETUP_PY
 
